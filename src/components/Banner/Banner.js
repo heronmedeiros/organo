c829b6d1bd46a084a4bf31b2015bbf3a2d4006fb
Heron Medeiros <heron.medeiros@gmail.com>
2023-10-01 08:01:03 -0300

    Removing extra files and create Banner

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,11 @@
+import './Banner.css'
+function Banner() {
+  // JSX
+  return (
+    <header className='banner'>
+      <img src="/images/banner.png" alt="O banner principal da Organo."/>
+    </header>
+  )
+}
+
+export default Banner;
